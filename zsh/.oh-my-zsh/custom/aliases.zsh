69fcc05f2579b62b0218fe4ce4b4986b420977c1
abdulkareemakn <abdulkareemakn@proton.me>
2024-09-18 14:29:51 +0500

    Added aliases

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,19 @@
 alias zshrc="micro ~/.zshrc"
-alias ls="exa -l --group-directories-first"
-alias la="exa -Al --group-directories-first"
+
+# File System Aliases
+alias ls="exa -lh --group-directories-first"
+alias la="exa -Alh --group-directories-first"
+
 alias cat="bat"
+
 alias ..="cd .."
+alias ...="cd ../.."
+
+alias mkdir="mkdir -pv"
+
+# pacman aliases
+alias upgrade="sudo pacman -Syu"
+alias install="sudo pacman -S"
+alias search="yay -Ss"
+
+ 
