62a4ee0577a00d310513a0d4f7cb1ba5e5b63c6d
abdulkareemakn <abdulkareemakn@proton.me>
2024-10-12 18:35:06 +0500

    configured bash formatter and codesnap.nvim

---
+++ neovim/.config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		config = function()
+			require("codesnap").setup({
+				{
+					mac_window_bar = true,
+					title = "CodeSnap.nvim",
+					code_font_family = "FiraCode Nerd Font",
+					watermark_font_family = "Pacifico",
+					watermark = "",
+					bg_theme = "bamboo",
+					breadcrumbs_separator = "/",
+					has_breadcrumbs = false,
+					has_line_number = false,
+					show_workspace = false,
+					min_width = 0,
+					bg_x_padding = 122,
+					bg_y_padding = 82,
+					save_path = os.getenv("XDG_PICTURES_DIR") or (os.getenv("HOME") .. "/Pictures"),
+				},
+			})
+		end,
+	},
+}
--- neovim/.config/nvim/lua/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/plugins/none-ls.lua
@@ -3,15 +3,18 @@ return {
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
+			debug = true,
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.shfmt,
+				--null_ls.builtins.formatting.beautysh,
 				--null_ls.builtins.diagnostics.eslint,
 				--null_ls.builtins.diagnostics.ruff,
 				--null_ls.builtins.formatting.ruff,
 				--null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.completion.spell,
+				--null_ls.builtins.completion.spell,
 			},
 		})
 		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
