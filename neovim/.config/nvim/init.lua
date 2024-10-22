f88b9e4dae9836ec5d2ff43b0522b163044c6442
abdulkareemakn <abdulkareemakn@proton.me>
2024-10-22 15:05:34 +0500

    Relative line numbers and theme

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 vim.g.mapleader = " "
-vim.opt.number = true
+vim.opt.relativenumber = true
 vim.keymap.set("n", "<leader>rc", " :!ruff check")
 vim.cmd("set guifont=FiraCode:12")
 vim.cmd("set termguicolors")
