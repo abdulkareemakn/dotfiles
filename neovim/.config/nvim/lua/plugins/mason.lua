da75dd69e7a00fb3320bf7d57a740f42881aa555
abdulkareemakn <abdulkareemakn@proton.me>
2024-10-27 15:14:20 +0500

    Added keybindings

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -107,6 +107,9 @@ return {
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "rn", vim.lsp.buf.rename, {})
+
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
