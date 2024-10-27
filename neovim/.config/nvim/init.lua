9e275a1e966fbb0e7df1a9a1e42bc392b587cdc1
abdulkareemakn <abdulkareemakn@proton.me>
2024-10-27 19:13:46 +0500

    Added keybindings to insert line breaks

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -4,3 +4,5 @@ vim.opt.relativenumber = true
 vim.keymap.set("n", "<leader>rc", " :!ruff check")
 vim.cmd("set guifont=FiraCode:12")
 vim.cmd("set termguicolors")
+vim.keymap.set("n", "<CR>", "m`o<Esc>``")
+vim.keymap.set("n", "<S-CR>", "m`O<Esc>``")
