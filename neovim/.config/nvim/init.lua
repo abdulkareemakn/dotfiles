b6aca20e4ca8cc0c7f6e2183a1793f6c01c6591d
abdulkareemakn <abdulkareemakn@proton.me>
2024-11-14 23:50:15 +0500

    reordered the file

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,8 +1,18 @@
+-- Lazy.nvim
 require("config.lazy")
+
+-- Leader
 vim.g.mapleader = " "
+
+-- Relative Line Numbers
 vim.opt.relativenumber = true
-vim.keymap.set("n", "<leader>rc", " :!ruff check")
-vim.cmd("set guifont=FiraCode:12")
+
+-- Colors
 vim.cmd("set termguicolors")
+
+-- Linebreaks
 vim.keymap.set("n", "<CR>", "m`o<Esc>``")
 vim.keymap.set("n", "<S-CR>", "m`O<Esc>``")
+
+-- colorscheme
+vim.cmd("colorscheme catppuccin-latte")
