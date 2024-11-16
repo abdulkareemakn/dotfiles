30d955cad9ddcf1cfa906118839d5954680441cc
abdulkareemakn <abdulkareemakn@proton.me>
2024-11-16 22:46:24 +0500

    added new theme

--- neovim/.config/nvim/lua/plugins/themery.lua
+++ neovim/.config/nvim/lua/plugins/themery.lua
@@ -9,6 +9,7 @@ return {
 				{ name = "Catppuccin", colorscheme = "catppuccin-latte" },
 				{ name = "Material", colorscheme = "material", before = [[vim.g.material_style = "lighter"]] },
 				{ name = "Ayu Light", colorscheme = "ayu", before = [[vim.g.ayucolor = "light"]] },
+				{ name = "TokyoNight", colorscheme = "tokyonight-day" },
 
 				{ name = "Monokai", colorscheme = "monokai-pro" },
 				{ name = "Ayu Mirage", colorscheme = "ayu", before = [[vim.g.ayucolor = "mirage"]] },
