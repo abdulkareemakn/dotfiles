68ba254541cbb09ec3229910e4733388c14a7684
abdulkareemakn <abdulkareemakn@proton.me>
2024-11-16 23:30:02 +0500

    new plugin for comments

--- neovim/.config/nvim/lua/plugins/comments.lua
+++ neovim/.config/nvim/lua/plugins/comments.lua
@@ -1,6 +1,7 @@
 return -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 {
 	"numToStr/Comment.nvim",
+	enabled = false,
 	opts = {
 		-- add any options here
 	},
