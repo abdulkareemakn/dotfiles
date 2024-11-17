45126ba03b6018df8901e07dcde2d3cd3d8584b8
abdulkareemakn <abdulkareemakn@proton.me>
2024-11-18 00:12:53 +0500

    added wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,37 @@
+-- Wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "MaterialDesignColors"
+
+-- Cursor
+config.default_cursor_style = "SteadyBar"
+
+-- Title Bar
+config.window_decorations = "RESIZE"
+
+-- Tab Bar
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Opacity
+config.window_background_opacity = 1.0
+
+-- Font
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", italic = false })
+
+-- Full Screen
+config.keys = {
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
