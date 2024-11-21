12061f80ecf461f965bfdb489577ff2a39e757c3
abdulkareemakn <abdulkareemakn@proton.me>
2024-11-21 14:45:40 +0500

    added tokyonight themes

--- neovim/.config/nvim/lua/plugins/themery.lua
+++ neovim/.config/nvim/lua/plugins/themery.lua
@@ -7,12 +7,18 @@ return {
 
 			themes = {
 				{ name = "Catppuccin", colorscheme = "catppuccin-latte" },
-				{ name = "Material", colorscheme = "material", before = [[vim.g.material_style = "lighter"]] },
+				{
+					name = "Material",
+					colorscheme = "material",
+					before = [[vim.g.material_style = "lighter"]],
+				},
 				{ name = "Ayu Light", colorscheme = "ayu", before = [[vim.g.ayucolor = "light"]] },
 				{ name = "TokyoNight", colorscheme = "tokyonight-day" },
 
 				{ name = "Monokai", colorscheme = "monokai-pro" },
 				{ name = "Ayu Mirage", colorscheme = "ayu", before = [[vim.g.ayucolor = "mirage"]] },
+				{ name = "TokyoNight Storm", colorscheme = "tokyonight-storm" },
+				{ name = "TokyoNight Moon", colorscheme = "tokyonight-moon" },
 			},
 		})
 	end,
