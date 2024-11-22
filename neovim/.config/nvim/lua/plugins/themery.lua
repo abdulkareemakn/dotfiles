049bcd91897ece2ee7cc8aefb2cc9b51c378cf93
abdulkareemakn <abdulkareemakn@proton.me>
2024-11-22 20:26:49 +0500

    installed a new plugin to track time and reordered plugins in theme file

--- neovim/.config/nvim/lua/plugins/themery.lua
+++ neovim/.config/nvim/lua/plugins/themery.lua
@@ -6,19 +6,20 @@ return {
 			livePreview = true,
 
 			themes = {
+
+				{ name = "Ayu Light", colorscheme = "ayu", before = [[vim.g.ayucolor = "light"]] },
 				{ name = "Catppuccin", colorscheme = "catppuccin-latte" },
 				{
 					name = "Material",
 					colorscheme = "material",
 					before = [[vim.g.material_style = "lighter"]],
 				},
-				{ name = "Ayu Light", colorscheme = "ayu", before = [[vim.g.ayucolor = "light"]] },
 				{ name = "TokyoNight", colorscheme = "tokyonight-day" },
 
-				{ name = "Monokai", colorscheme = "monokai-pro" },
 				{ name = "Ayu Mirage", colorscheme = "ayu", before = [[vim.g.ayucolor = "mirage"]] },
-				{ name = "TokyoNight Storm", colorscheme = "tokyonight-storm" },
+				{ name = "Monokai", colorscheme = "monokai-pro" },
 				{ name = "TokyoNight Moon", colorscheme = "tokyonight-moon" },
+				{ name = "TokyoNight Storm", colorscheme = "tokyonight-storm" },
 			},
 		})
 	end,
