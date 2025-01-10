c231f59fd2b15eefa326fbef42ecb3f661b7cc1c
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-10 14:55:03 +0500

    removed luasnip from default sources

--- neovim/.config/nvim/lua/plugins/completions.lua
+++ neovim/.config/nvim/lua/plugins/completions.lua
@@ -32,7 +32,8 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "luasnip", "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets", "buffer" },
+			--default = { "luasnip", "lsp", "path", "snippets", "buffer" },
 		},
 	},
 	opts_extend = { "sources.default" },
