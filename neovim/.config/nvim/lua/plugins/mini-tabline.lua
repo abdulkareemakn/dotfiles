7222787834a450cb0c70f56c9d5c1ade6ba1438b
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-14 14:15:08 +0500

    disabled plugin and removed keymaps to place in global keymaps file

--- neovim/.config/nvim/lua/plugins/mini-tabline.lua
+++ neovim/.config/nvim/lua/plugins/mini-tabline.lua
@@ -1,18 +1,8 @@
 return {
-	"echasnovski/mini.tabline",
-	version = false,
-	config = function()
-		require("mini.tabline").setup({})
-		-- Navigate to the next buffer
-		vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
-
-		-- Navigate to the previous buffer
-		vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
-
-		-- Close the current buffer
-		vim.keymap.set("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete buffer" })
-
-		-- Pick a buffer from the buffer list
-		vim.keymap.set("n", "<leader>bb", ":ls<CR>:b<Space>", { desc = "Pick buffer" })
-	end,
+    "echasnovski/mini.tabline",
+    version = false,
+    enabled = false,
+    config = function()
+        require("mini.tabline").setup({})
+    end,
 }
