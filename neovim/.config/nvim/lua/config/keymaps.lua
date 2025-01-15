dbb5775a6f2bc1914c60221628e9bc3af7caa5a4
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-15 11:55:35 +0500

    added a bunch of keybindings for snacks pickers

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -16,11 +16,27 @@ map("i", "jk", "<Esc>", { desc = "Switch to normal Mode" })
 map("i", "kj", "<Esc>", { desc = "Switch to normal Mode" })
 
 -- Buffers
-map("n", "<leader>bb", ":Telescope buffers<CR>", { desc = "See open buffers" })
+-- map("n", "<leader>bb", ":Telescope buffers<CR>", { desc = "See open buffers" })
+map("n", "<leader>bb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
 map("n", "<leader>bn", ":bnext<CR>", { desc = "Next Buffer" })
 map("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous Buffer" })
 map("n", "<leader>bd", ":bdelete<CR>", { desc = "Delete Buffer" })
 
+-- Files
+map("n", "<C-f>", ":lua MiniFiles.open()<CR>", { desc = "File Explorer" })
+map("n", "<leader><space>", function() Snacks.picker.files() end, { desc = "Find Files" })
+map("n", "<leader>fg", function() Snacks.picker.grep() end, { desc = "Find Files" })
+
+map("n", "<leader>sd", function() Snacks.picker.diagnostics() end, { desc = "Diagnostics" })
+
+map("n", "<leader>ht", function() Snacks.picker.colorschemes() end, { desc = "Diagnostics" })
+
+-- LSP
+map("n", "gd", function() Snacks.picker.lsp_definitions() end, { desc = "Goto Definition" })
+map("n", "gI", function() Snacks.picker.lsp_implementations() end, { desc = "Goto Implementation" })
+map("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "LSP Symbols" })
+
+-- Pickers
 
 
 -- map("v", "<S-A-Down>", "<Esc>yp", { desc = "Clone code block down" })
