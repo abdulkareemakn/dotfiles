107dbe84174dc54ffb293f945e09845e88abf50e
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-16 13:48:34 +0500

    added a function to mock nvim-web-devicons

--- neovim/.config/nvim/lua/plugins/mini-icons.lua
+++ neovim/.config/nvim/lua/plugins/mini-icons.lua
@@ -1,6 +1,14 @@
 return {
 	"echasnovski/mini.icons",
 	version = false,
+
+	init = function()
+		package.preload["nvim-web-devicons"] = function()
+			require("mini.icons").mock_nvim_web_devicons()
+			return package.loaded["nvim-web-devicons"]
+		end
+	end,
+
 	config = function()
 		require("mini.icons").setup()
 	end,
