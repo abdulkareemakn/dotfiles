7797f161e2a8f1c0e570b5f0ccecd3a2100e005f
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-20 19:00:47 +0500

    changed keybindings and configured lualine

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -6,8 +6,10 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				-- component_separators = { left = "", right = "" },
+				component_separators = { left = "|", right = "|" },
+				-- section_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
@@ -27,8 +29,10 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
+				-- lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = { "fileformat", "filetype" },
+				-- lualine_y = { "progress" },
+				lualine_y = { "" },
 				lualine_z = { "location" },
 			},
 
