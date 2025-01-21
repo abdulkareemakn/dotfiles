1873f07a5fdd38457f2b620b939a7e050e3f1efd
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-21 15:00:34 +0500

    added kitty syntax highlight and filetype setting.

--- neovim/.config/nvim/lua/plugins/emmet.lua
+++ neovim/.config/nvim/lua/plugins/emmet.lua
@@ -1,6 +1,7 @@
 return {
-    "olrtg/nvim-emmet",
-    config = function()
-        vim.keymap.set({ "n", "v" }, "<leader>ee", require("nvim-emmet").wrap_with_abbreviation)
-    end,
+	"olrtg/nvim-emmet",
+	ft = { "html" },
+	config = function()
+		vim.keymap.set({ "n", "v" }, "<leader>ee", require("nvim-emmet").wrap_with_abbreviation)
+	end,
 }
--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,22 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	version=false,
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false,
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+            configs.setup({
+                auto_install = true,
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "fladson/vim-kitty",
+        ft = "kitty",
+        tag = "*", -- You can select a tagged version
+    },
 }
