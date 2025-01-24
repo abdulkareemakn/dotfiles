9daedbe67cba5c9f44dcc8aa6cb5507c6fe46f22
abdulkareemakn <abdulkareemakn@proton.me>
2025-01-24 19:08:39 +0500

    added scroll and changed colorscheme

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -1,22 +1,23 @@
 return {
-    "folke/snacks.nvim",
-    priority = 500,
-    lazy = false,
-    opts = {
-        bigfile = { enabled = true },
-        dashboard = { enabled = true },
-        input = { enabled = true },
-        quickfile = { enabled = true },
-        picker = {
-            enabled = true,
-            formatters = {
-                file = {
-                    filename_first = true,
-                },
-            },
-        },
-        notifier = { enabled = true },
-        scope = { enabled = true },
-        terminal = { enabled = true },
-    },
+	"folke/snacks.nvim",
+	priority = 500,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = { enabled = true },
+		input = { enabled = true },
+		quickfile = { enabled = true },
+		picker = {
+			enabled = true,
+			formatters = {
+				file = {
+					filename_first = true,
+				},
+			},
+		},
+		notifier = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = true },
+		terminal = { enabled = true },
+	},
 }
