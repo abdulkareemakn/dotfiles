61dc9dd7f63eeb2bcf40bf77feceff8f4ea7eb03
abdulkareemakn <abdulkareemakn@proton.me>
2025-02-01 02:19:53 +0500

    font and theme config done.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,11 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "MaterialDesign"
+config.color_scheme = "Aura"
 -- config.color_scheme = "Material (base16)"
 
 -- Cursor
 config.default_cursor_style = "SteadyBar"
+config.cursor_thickness = "2"
 -- config.default_cursor_style = "BlinkingBlock"
 
 -- Title Bar
@@ -23,19 +24,10 @@ config.hide_tab_bar_if_only_one_tab = true
 -- Opacity
 config.window_background_opacity = 1.0
 
--- Font
--- config.font = wezterm.font({
--- 	family = "MonaspiceNeNF-Medium",
--- 	harfbuzz_features = { "calt", "liga", "ss01", "ss03", "ss06", "ss07", "ss08", "ss09" },
--- })
-
--- config.font = wezterm.font({
--- 	family = "CaskaydiaCove Nerd Font",
--- 	weight = "Bold",
--- })
-
 -- config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" })
 config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "DemiBold", stretch = "Normal", style = "Normal" })
+
+config.harfbuzz_features = { "ss19", "ss20" }
 
 config.font_size = 12.5
 
