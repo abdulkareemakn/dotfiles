22161f37ed424c7b9ee29ff0e9e99d6ac6c34865
abdulkareemakn <abdulkareemakn@proton.me>
2025-02-01 19:03:22 +0500

    modified dashboard and added trouble (disabled)

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -5,7 +5,42 @@ return {
 	opts = {
 		animate = { enabled = true },
 		bigfile = { enabled = true },
-		dashboard = { enabled = true },
+		dashboard = {
+			preset = {
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = "󰙅", key = "e", desc = "File Explorer", action = ":lua Snacks.picker.explorer()" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{
+						icon = "󰒲 ",
+						key = "L",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+			},
+		},
+		explorer = { enabled = true },
 		input = { enabled = true },
 		lazygit = { enabled = true },
 		notifier = { enabled = true },
@@ -21,6 +56,13 @@ return {
 		rename = { enabled = true },
 		scope = { enabled = true },
 		scroll = { enabled = true },
-		terminal = { enabled = true },
+		terminal = {
+			enabled = true,
+			win = {
+				wo = {
+					winbar = "",
+				},
+			},
+		},
 	},
 }
