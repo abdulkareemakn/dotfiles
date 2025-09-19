47236d5aefec472ff34efdfca3a03c744812d202
Abdul Kareem <abdulkareemakn@proton.me>
2025-09-19 22:56:36 +0500

    removed mason and refactored lsp

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,175 +1,90 @@
 return {
+	{
+		"neovim/nvim-lspconfig",
 
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                -- ensure_installed = {
-                --     "clang-format",
-                --     "stylua",
-                --     "prettierd",
-                --     "shfmt",
-                -- },
-            })
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                -- ensure_installed = {
-                --     -- LSPs
-                --     "lua_ls",
-                --     -- "clangd",
-                --     "basedpyright",
-                --     "ruff",
-                --     "ts_ls",
-                --     "taplo",
-                --     "gopls",
-                --     "jsonls",
-                --     "yamlls"
-                -- },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
-            local on_attach =
-                function(client, bufnr)
-                    -- LSP keybindings
-                    local opts = { noremap = true, silent = true, buffer = bufnr }
+			local lspconfig = require("lspconfig")
+			local on_attach = function(client, bufnr)
+				-- LSP keybindings
+				local opts = { noremap = true, silent = true, buffer = bufnr }
 
-                    -- Go to definition
-                    --					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts, { desc = "Goto Definition" })
+				-- Go to definition
+				--					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts, { desc = "Goto Definition" })
 
-                    -- Go to declaration
-                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts, { desc = "Goto Declaration" })
+				-- Go to declaration
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts, { desc = "Goto Declaration" })
 
-                    -- Go to type definition
-                    vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+				-- Go to type definition
+				vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
 
-                    -- Go to implementation
-                    --					vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+				-- Go to implementation
+				--					vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
 
-                    -- Show hover documentation
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				-- Show hover documentation
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-                    -- Show signature help
-                    vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set("i", "<c-k>", vim.lsp.buf.signature_help, opts)
+				-- Show signature help
+				vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("i", "<c-k>", vim.lsp.buf.signature_help, opts)
 
-                    -- Renaming
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				-- Renaming
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-                    -- List references
-                    -- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				-- List references
+				-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-                    -- Show diagnostic
-                    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+				-- Show diagnostic
+				vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
 
-                    -- Go to next diagnostic
-                    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				-- Go to next diagnostic
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
-                    -- Go to previous diagnostic
-                    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+				-- Go to previous diagnostic
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
-                    -- Show diagnostic list
-                    vim.keymap.set("n", "<leader>dl", vim.diagnostic.setqflist, opts)
+				-- Show diagnostic list
+				vim.keymap.set("n", "<leader>dl", vim.diagnostic.setqflist, opts)
 
-                    -- Show code actions (e.g., fix code)
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				-- Show code actions (e.g., fix code)
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-                    -- Show document symbols
-                    -- vim.keymap.set("n", "<leader>ds", vim.lsp.buf.document_symbol, opts)
+				-- Show document symbols
+				-- vim.keymap.set("n", "<leader>ds", vim.lsp.buf.document_symbol, opts)
 
-                    -- Show workspace symbols
-                    vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
-                end, lspconfig.lua_ls.setup({
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                })
+				-- Show workspace symbols
+				vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+			end
 
-            lspconfig.clangd.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.basedpyright.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                settings = {
-                    basedpyright = {
-                        disableOrganizeImports = true,
-                    },
-                    python = {
-                        analysis = {
-                            ignore = { "*" },
-                        },
-                    },
-                },
-            })
-            lspconfig.ts_ls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.taplo.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.ruff.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.basedpyright.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.gopls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.jsonls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.emmet_language_server.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.jdtls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
-            lspconfig.yamlls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-            })
+			local servers = {
+				"lua_ls",
+				"clangd",
+				"jsonls",
+				"yamlls",
+			}
 
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
-                callback = function(args)
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client == nil then
-                        return
-                    end
-                    if client.name == "ruff" then
-                        -- Disable hover in favor of Pyright
-                        client.server_capabilities.hoverProvider = false
-                    end
-                end,
-                desc = "LSP: Disable hover capability from Ruff",
-            })
-        end,
-    },
+			for _, server in ipairs(servers) do
+				vim.lsp.config(server, {
+					on_attach = on_attach,
+					capabilities = capabilities,
+				})
+			end
+
+			-- 	vim.api.nvim_create_autocmd("LspAttach", {
+			-- 		group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+			-- 		callback = function(args)
+			-- 			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			-- 			if client == nil then
+			-- 				return
+			-- 			end
+			-- 			if client.name == "ruff" then
+			-- 				-- Disable hover in favor of Pyright
+			-- 				client.server_capabilities.hoverProvider = false
+			-- 			end
+			-- 		end,
+			-- 		desc = "LSP: Disable hover capability from Ruff",
+			-- 	})
+		end,
+	},
 }
