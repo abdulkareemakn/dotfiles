880d395392413e286ec7aee696e825637fa5006f
Abdul Kareem <abdulkareemakn@proton.me>
2025-10-08 16:17:11 +0500

    removed friendly snippets and emmet

--- neovim/.config/nvim/lua/plugins/completions.lua
+++ neovim/.config/nvim/lua/plugins/completions.lua
@@ -1,23 +1,12 @@
 return {
 	"saghen/blink.cmp",
 	event = "InsertEnter",
-	-- optional: provides snippets for the snippet source
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		--        "L3MON4D3/LuaSnip",
-		version = "v2.*",
-		{
-			"giuxtaposition/blink-cmp-copilot",
-		},
-	},
-
 	-- use a release tag to download pre-built binaries
 	version = "*",
 	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 	-- build = 'cargo build --release',
 	-- If you use nix, you can build from source using latest nightly rust with:
 	-- build = 'nix run .#build-plugin',
-
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
@@ -32,12 +21,18 @@ return {
 				draw = {
 					components = {
 						kind_icon = {
-							ellipsis = false,
 							text = function(ctx)
 								local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
 								return kind_icon
 							end,
-							-- Optionally, you may also use the highlights from mini.icons
+							-- (optional) use highlights from mini.icons
+							highlight = function(ctx)
+								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								return hl
+							end,
+						},
+						kind = {
+							-- (optional) use highlights from mini.icons
 							highlight = function(ctx)
 								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
 								return hl
@@ -45,19 +40,11 @@ return {
 						},
 					},
 				},
-			},
+			}, -- <-- This closing brace was missing
 		},
-
 		appearance = {
-			-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-			-- Useful for when your theme doesn't support blink.cmp
-			-- Will be removed in a future release
-			use_nvim_cmp_as_default = true,
-			-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "normal",
 		},
-
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
@@ -67,24 +54,8 @@ return {
 						search_paths = { "/home/abdulkareem/.config/nvim/snippets/" },
 					},
 				},
-				-- 	copilot = {
-				-- 		name = "copilot",
-				-- 		module = "blink-cmp-copilot",
-				-- 		score_offset = 100,
-				-- 		async = true,
-				-- 		transform_items = function(_, items)
-				-- 			local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-				-- 			local kind_idx = #CompletionItemKind + 1
-				-- 			CompletionItemKind[kind_idx] = "Copilot"
-				-- 			for _, item in ipairs(items) do
-				-- 				item.kind = kind_idx
-				-- 			end
-				-- 			return items
-				-- 		end,
-				-- 	},
 			},
 			default = { "lsp", "path", "snippets", "buffer" },
-			--default = { "luasnip", "lsp", "path", "snippets", "buffer" },
 		},
 	},
 	opts_extend = { "sources.default" },
--- neovim/.config/nvim/lua/plugins/emmet.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"olrtg/nvim-emmet",
-    enabled = false,
-	ft = { "html" },
-	event = "VeryLazy",
-	config = function()
-		vim.keymap.set({ "n", "v" }, "<leader>ee", require("nvim-emmet").wrap_with_abbreviation)
-	end,
-}
