e653e3f9527b221a5ffa16835b90e9368ecc75e1
Abdul Kareem <abdulkareemakn@proton.me>
2026-02-17 12:43:43 +0500

    Updated theme to TokyoNight

--- nvim/.config/nvim/lua/plugins/themery.lua
+++ nvim/.config/nvim/lua/plugins/themery.lua
@@ -1,161 +1,161 @@
 return {
-	"zaldih/themery.nvim",
-	lazy = false,
-	config = function()
-		require("themery").setup({
-			themes = {
-				-- {
-				-- 	name = "Ayu Light",
-				-- 	colorscheme = "ayu",
-				-- 	before = [[vim.g.ayucolor="light"]],
-				-- },
+    "zaldih/themery.nvim",
+    lazy = false,
+    config = function()
+        require("themery").setup({
+            themes = {
+                -- {
+                -- 	name = "Ayu Light",
+                -- 	colorscheme = "ayu",
+                -- 	before = [[vim.g.ayucolor="light"]],
+                -- },
 
-				-- { name = "Catppuccin Latte", colorscheme = "catppuccin-latte" },
+                -- { name = "Catppuccin Latte", colorscheme = "catppuccin-latte" },
 
-				-- { name = "Dayfox",                     colorscheme = "dayfox" },
+                -- { name = "Dayfox",                     colorscheme = "dayfox" },
 
-				-- { name = "Github Light", colorscheme = "github_light" },
-				-- { name = "Github Light Default", colorscheme = "github_light_default" },
-				-- { name = "Github Light High Contrast", colorscheme = "github_light_high_contrast" },
-				-- { name = "Github Light Colorblind", colorscheme = "github_light_colorblind" },
-				-- { name = "Github Light Tritanopia", colorscheme = "github_light_tritanopia" },
+                -- { name = "Github Light", colorscheme = "github_light" },
+                -- { name = "Github Light Default", colorscheme = "github_light_default" },
+                -- { name = "Github Light High Contrast", colorscheme = "github_light_high_contrast" },
+                -- { name = "Github Light Colorblind", colorscheme = "github_light_colorblind" },
+                -- { name = "Github Light Tritanopia", colorscheme = "github_light_tritanopia" },
 
-				-- {
-				-- 	name = "Gruvbox Light",
-				-- 	colorscheme = "gruvbox",
-				-- 	before = [[vim.o.background="light"]],
-				-- },
+                -- {
+                -- 	name = "Gruvbox Light",
+                -- 	colorscheme = "gruvbox",
+                -- 	before = [[vim.o.background="light"]],
+                -- },
 
-				-- { name = "Kanagawa Lotus", colorscheme = "kanagawa-lotus" },
+                -- { name = "Kanagawa Lotus", colorscheme = "kanagawa-lotus" },
 
-				{
-					name = "Material Lighter",
-					colorscheme = "material",
-					before = [[vim.g.material_style="lighter"]],
-				},
+                {
+                    name = "Material Lighter",
+                    colorscheme = "material",
+                    before = [[vim.g.material_style="lighter"]],
+                },
 
-				-- { name = "OneLight", colorscheme = "onelight" },
+                -- { name = "OneLight", colorscheme = "onelight" },
 
-				-- { name = "Rose Pine Dawn", colorscheme = "rose-pine-dawn" },
+                -- { name = "Rose Pine Dawn", colorscheme = "rose-pine-dawn" },
 
-				-- {
-				-- 	name = "Ayu Dark",
-				-- 	colorscheme = "ayu",
-				-- 	before = [[vim.g.ayucolor="dark"]],
-				-- },
-				-- {
-				-- 	name = "Ayu Mirage",
-				-- 	colorscheme = "ayu",
-				-- 	before = [[vim.g.ayucolor="mirage"]],
-				-- },
+                -- {
+                -- 	name = "Ayu Dark",
+                -- 	colorscheme = "ayu",
+                -- 	before = [[vim.g.ayucolor="dark"]],
+                -- },
+                -- {
+                -- 	name = "Ayu Mirage",
+                -- 	colorscheme = "ayu",
+                -- 	before = [[vim.g.ayucolor="mirage"]],
+                -- },
 
-				-- { name = "TokyoNight Day",             colorscheme = "tokyonight-day" },
+                { name = "TokyoNight Day",   colorscheme = "tokyonight-day" },
 
-				-- { name = "Carbonfox",                  colorscheme = "carbonfox" },
+                -- { name = "Carbonfox",                  colorscheme = "carbonfox" },
 
-				-- { name = "Catppuccin Frappe", colorscheme = "catppuccin-frappe" },
-				-- { name = "Catppuccin Macchiato", colorscheme = "catppuccin-macchiato" },
-				-- { name = "Catppuccin Mocha", colorscheme = "catppuccin-mocha" },
+                -- { name = "Catppuccin Frappe", colorscheme = "catppuccin-frappe" },
+                -- { name = "Catppuccin Macchiato", colorscheme = "catppuccin-macchiato" },
+                -- { name = "Catppuccin Mocha", colorscheme = "catppuccin-mocha" },
 
-				-- { name = "Dracula",                    colorscheme = "dracula" },
+                -- { name = "Dracula",                    colorscheme = "dracula" },
 
-				-- { name = "Github Dark", colorscheme = "github_dark" },
-				-- { name = "Github Dark Dimmed", colorscheme = "github_dark_dimmed" },
-				-- { name = "Github Dark Default", colorscheme = "github_dark_default" },
-				-- { name = "Github Dark High Contrast", colorscheme = "github_dark_high_contrast" },
-				-- { name = "Github Dark Colorblink", colorscheme = "github_dark_colorblind" },
-				-- { name = "Github Dark Tritanopia", colorscheme = "github_dark_tritanopia" },
+                -- { name = "Github Dark", colorscheme = "github_dark" },
+                -- { name = "Github Dark Dimmed", colorscheme = "github_dark_dimmed" },
+                -- { name = "Github Dark Default", colorscheme = "github_dark_default" },
+                -- { name = "Github Dark High Contrast", colorscheme = "github_dark_high_contrast" },
+                -- { name = "Github Dark Colorblink", colorscheme = "github_dark_colorblind" },
+                -- { name = "Github Dark Tritanopia", colorscheme = "github_dark_tritanopia" },
 
-				-- {
-				-- 	name = "Gruvbox Dark",
-				-- 	colorscheme = "gruvbox",
-				-- 	before = [[vim.o.background="dark"]],
-				-- },
+                -- {
+                -- 	name = "Gruvbox Dark",
+                -- 	colorscheme = "gruvbox",
+                -- 	before = [[vim.o.background="dark"]],
+                -- },
 
-				-- { name = "Halcyon", colorscheme = "halcyon" },
+                -- { name = "Halcyon", colorscheme = "halcyon" },
 
-				-- { name = "Kanagawa Wave", colorscheme = "kanagawa-wave" },
-				-- { name = "Kanagawa Dragon", colorscheme = "kanagawa-dragon" },
+                -- { name = "Kanagawa Wave", colorscheme = "kanagawa-wave" },
+                -- { name = "Kanagawa Dragon", colorscheme = "kanagawa-dragon" },
 
-				-- {
-				-- 	name = "Material Darker",
-				-- 	colorscheme = "material",
-				-- 	before = [[vim.g.material_style="darker"]],
-				-- },
-				--
-				-- {
-				-- 	name = "Material Oceanic",
-				-- 	colorscheme = "material",
-				-- 	before = [[vim.g.material_style="oceanic"]],
-				-- },
-				--
-				-- {
-				-- 	name = "Material Palenight",
-				-- 	colorscheme = "material",
-				-- 	before = [[vim.g.material_style="palenight"]],
-				-- },
-				--
-				-- {
-				-- 	name = "Material Deep Ocean",
-				-- 	colorscheme = "material",
-				-- 	before = [[vim.g.material_style="deep ocean"]],
-				-- },
+                -- {
+                -- 	name = "Material Darker",
+                -- 	colorscheme = "material",
+                -- 	before = [[vim.g.material_style="darker"]],
+                -- },
+                --
+                -- {
+                -- 	name = "Material Oceanic",
+                -- 	colorscheme = "material",
+                -- 	before = [[vim.g.material_style="oceanic"]],
+                -- },
+                --
+                -- {
+                -- 	name = "Material Palenight",
+                -- 	colorscheme = "material",
+                -- 	before = [[vim.g.material_style="palenight"]],
+                -- },
+                --
+                -- {
+                -- 	name = "Material Deep Ocean",
+                -- 	colorscheme = "material",
+                -- 	before = [[vim.g.material_style="deep ocean"]],
+                -- },
 
-				{
-					name = "Monokai Pro",
-					colorscheme = "monokai-pro",
-					before = [[require("monokai-pro").setup({filter="pro"})]],
-				},
-				{
-					name = "Monokai Pro Octagon",
-					colorscheme = "monokai-pro",
-					before = [[require("monokai-pro").setup({filter="octagon"})]],
-				},
-				{
-					name = "Monokai Pro Machine",
-					colorscheme = "monokai-pro",
-					before = [[require("monokai-pro").setup({filter="machine"})]],
-				},
-				{
-					name = "Monokai Pro Ristretto",
-					colorscheme = "monokai-pro",
-					before = [[require("monokai-pro").setup({filter="ristretto"})]],
-				},
+                {
+                    name = "Monokai Pro",
+                    colorscheme = "monokai-pro",
+                    before = [[require("monokai-pro").setup({filter="pro"})]],
+                },
+                {
+                    name = "Monokai Pro Octagon",
+                    colorscheme = "monokai-pro",
+                    before = [[require("monokai-pro").setup({filter="octagon"})]],
+                },
+                {
+                    name = "Monokai Pro Machine",
+                    colorscheme = "monokai-pro",
+                    before = [[require("monokai-pro").setup({filter="machine"})]],
+                },
+                {
+                    name = "Monokai Pro Ristretto",
+                    colorscheme = "monokai-pro",
+                    before = [[require("monokai-pro").setup({filter="ristretto"})]],
+                },
 
-				{
-					name = "Monokai Pro Spectrum",
-					colorscheme = "monokai-pro",
-					before = [[require("monokai-pro").setup({filter="spectrum"})]],
-				},
+                {
+                    name = "Monokai Pro Spectrum",
+                    colorscheme = "monokai-pro",
+                    before = [[require("monokai-pro").setup({filter="spectrum"})]],
+                },
 
-				{
-					name = "Monokai Classic",
-					colorscheme = "monokai-pro",
-					before = [[require("monokai-pro").setup({filter="classic"})]],
-				},
+                {
+                    name = "Monokai Classic",
+                    colorscheme = "monokai-pro",
+                    before = [[require("monokai-pro").setup({filter="classic"})]],
+                },
 
-				-- { name = "Nord", colorscheme = "nord" },
+                -- { name = "Nord", colorscheme = "nord" },
 
-				-- { name = "Nightfox",         colorscheme = "nightfox" },
-				-- { name = "Nordfox",          colorscheme = "nordfox" },
+                -- { name = "Nightfox",         colorscheme = "nightfox" },
+                -- { name = "Nordfox",          colorscheme = "nordfox" },
 
-				-- { name = "OneDark", colorscheme = "onedark" },
-				-- { name = "OneDark Vivid", colorscheme = "onedark_vivid" },
-				-- { name = "OneDark Dark", colorscheme = "onedark_dark" },
+                -- { name = "OneDark", colorscheme = "onedark" },
+                -- { name = "OneDark Vivid", colorscheme = "onedark_vivid" },
+                -- { name = "OneDark Dark", colorscheme = "onedark_dark" },
 
-				-- { name = "Oxocarbon",        colorscheme = "oxocarbon" },
+                -- { name = "Oxocarbon",        colorscheme = "oxocarbon" },
 
-				-- { name = "Poimandres", colorscheme = "poimandres" },
+                -- { name = "Poimandres", colorscheme = "poimandres" },
 
-				-- { name = "Rose Pine", colorscheme = "rose-pine-main" },
-				-- { name = "Rose Pine Moon", colorscheme = "rose-pine-moon" },
+                -- { name = "Rose Pine", colorscheme = "rose-pine-main" },
+                -- { name = "Rose Pine Moon", colorscheme = "rose-pine-moon" },
 
-				-- { name = "Terafox",          colorscheme = "terafox" },
+                -- { name = "Terafox",          colorscheme = "terafox" },
 
-				-- { name = "TokyoNight Night", colorscheme = "tokyonight-night" },
-				-- { name = "TokyoNight Storm", colorscheme = "tokyonight-storm" },
-				-- { name = "TokyoNight Moon",  colorscheme = "tokyonight-moon" },
-			},
-		})
-	end,
+                { name = "TokyoNight Night", colorscheme = "tokyonight-night" },
+                { name = "TokyoNight Storm", colorscheme = "tokyonight-storm" },
+                { name = "TokyoNight Moon",  colorscheme = "tokyonight-moon" },
+            },
+        })
+    end,
 }
